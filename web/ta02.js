c41a23c703e9d8342fa18765108c9d05c393efe3
Brian_064 <jarelbrian064@gmail.com>
2019-05-01 23:40:22 -0600

    All requirements met

--- web/ta02.js
+++ web/ta02.js
@@ -1,5 +1,21 @@
 $(document).ready(main);
 
 function main() {
-  
+  $("body").hide();
+  $("body").fadeIn(2000);
+
+  //Function for changing first div color
+  $(".changeColor").click(function() {
+    $("#first").css("background-color", $("#color").val());
+  })
+
+  //Function for Clicked! alert
+  $("#clickMe").click(function() {
+    alert("Clicked!");
+  })
+
+  //Function toggles third div visibility
+  $("#toggle").click(function() {
+    $("#third").fadeToggle(500);
+  })
 }
