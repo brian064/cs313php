a76cf91bc440b212a8b4a9b3e69c0047b644d1b2
Brian_064 <jarelbrian064@gmail.com>
2019-05-02 18:23:44 -0600

    home page initial commit

---
+++ web/homepage.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Brian A's Homepage</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="baHome.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="container">
+    </div>
+
+    <!-- jQuery library script below -->
+    <script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+
+    <!-- personal JS file below -->
+    <script src="baHome.js"></script>
+  </body>
+</html>
