b7985b1b43d9e67f99a513f5e31be6bbbae08464
Brian_064 <jarelbrian064@gmail.com>
2019-05-22 15:35:48 -0600

    added ta05.php

---
+++ web/ta05.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Brian A's Homepage</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="baHome.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <div class="container">
+      <h1>Scripture Resources</h1>
+    </div>
+
+    <footer>
+    </footer>
+
+    <!-- jQuery library script below -->
+    <script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+
+    <!-- personal JS file below -->
+    <script src="baHome.js"></script>
+  </body>
+</html>
