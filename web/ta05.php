b545011864b58f6b40fb84b3f585a80d095c63cd
Brian_064 <jarelbrian064@gmail.com>
2019-05-22 15:44:00 -0600

    added ta05.php

--- web/ta05.php
+++ web/ta05.php
@@ -17,8 +17,43 @@
   </head>
 
   <body>
+    <!-- PHP section -->
+    <?php
+      //Connecting to Heroku Database
+      try
+      {
+        $dbUrl = getenv('DATABASE_URL');
+
+        $dbOpts = parse_url($dbUrl);
+
+        $dbHost = $dbOpts["host"];
+        $dbPort = $dbOpts["port"];
+        $dbUser = $dbOpts["user"];
+        $dbPassword = $dbOpts["pass"];
+        $dbName = ltrim($dbOpts["path"],'/');
+
+        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      }
+
+      catch (PDOException $ex)
+      {
+        echo 'Error!: ' . $ex->getMessage();
+        die();
+      }
+    ?>
     <div class="">
       <h1>Scripture Resources</h1>
+      <?php
+      foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures') as $row)
+      {
+        echo $row['book']. '';
+        echo $row['chapter'] . ':';
+        echo $row['verse'] . ' - \"';
+        echo $row['content'] . '\" <br/><br/>'
+      }
+      ?>
     </div>
 
     <footer>
