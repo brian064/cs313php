2a9636054da1eeaa52bdd62420f3207651cdbd18
Brian_064 <jarelbrian064@gmail.com>
2019-06-04 19:15:02 -0600

    update

--- web/Prove05/deleted.php
+++ web/Prove05/deleted.php
@@ -30,7 +30,13 @@ session_start();
       $_SESSION["usr"] = $_POST["usr"];
       }
 
+      //getting Form Variables
+      $trans = $_POST["trans"];
+      $user = $_SESSION["usr"];
+
       //SQL Select statements
+      $deleteSql = "DELETE FROM users WHERE usrname = '$user'";
+
       $sql = 'SELECT * FROM users WHERE usrname = \'' . $_SESSION["usr"] . '\'';
 
       $osql = 'SELECT (tprog+frprog+fnprog)/3 AS oprog FROM users WHERE usrname = \'' . $_SESSION["usr"] . '\'';
@@ -50,6 +56,9 @@ session_start();
         $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        //insert new user in users table
+        $db->exec($deleteSql);
       }
 
       catch (PDOException $ex)
@@ -61,62 +70,14 @@ session_start();
 
     <div class="myNav">
       <div class="container">
-        <a href="home.php"><h4>Back to Home</h4></a>
+        <a href="login.php"><h4>Back to Login</h4></a>
       </div>
     </div>
 
     <div class="cheader container">
       <h2>
-        <?php
-          foreach ($db->query($sql) as $row)
-          {
-            echo $row['firstn'] . " " . $row['lastn'];
-          }
-        ?>'s Profile
+        Deleted Your Profile!
       </h2>
-
-      <div class="homeButtons">
-        <a href="deleted.php"><button>Delete Your Profile</button></a>
-      </div>
-    </div>
-
-    <div class="container overall trans">
-      <h2>Age: <?php
-        foreach ($db->query($sql) as $row)
-        {
-          echo $row['age'];
-        }
-      ?></h2>
-
-      <h2>Bio: <?php
-        foreach ($db->query($sql) as $row)
-        {
-          echo $row['bio'];
-        }
-      ?></h2>
-    </div>
-
-    <div class="container updateForm form">
-      <h2>Update Your Skill Progress</h2><br/><br/>
-      <p>Enter a number between 0 - 100</p><br/>
-
-      <form action="transUpdate.php" method="post">
-        Transition Skating: <input type="number" max="100" name="trans"/><br/>
-        <button type="submit">Update</button>
-        <br/><br/>
-      </form>
-
-      <form action="freeUpdate.php" method="post">
-        Freestyle Skating: <input type="number" max="100" name="free"/><br/>
-        <button type="submit">Update</button>
-        <br/><br/>
-      </form>
-
-      <form action="funUpdate.php" method="post">
-        Fundamentals Skating: <input type="number" max="100" name="fun"/><br/>
-        <button type="submit">Update</button>
-        <br/><br/>
-      </form>
     </div>
 
     <footer>
